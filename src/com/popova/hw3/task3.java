a816e08f04eceb2215b52f0cd76f5cbb9d1dfd2a
Kate popova <katenysh98@gmail.com>
2021-11-03 21:58:41 +0200

    исправила hw3

--- src/com/popova/hw3/task3.java
+++ src/com/popova/hw3/task3.java
@@ -3,11 +3,11 @@ package com.popova.hw3;
 //добавила секунды к этому делу
 public class task3 {
     public static void main(String[] args) {
-        for (int hour = 0; hour < 23; hour++) {
+        for (int hour = 0; hour < 24; hour++) {
             String hourS = hour < 10 ? "0" + hour : String.valueOf(hour);
-            for (int min = 0; min < 59; min++) {
+            for (int min = 0; min < 60; min++) {
                 String minS = min < 10 ? "0" + min : String.valueOf(min);
-                for (int second = 0; second < 59; second++) {
+                for (int second = 0; second < 60; second++) {
                     String secondS = second < 10 ? "0" + second : String.valueOf(second);
                     if (hourS.equals(minS)) {
                         StringBuilder stringMin = new StringBuilder(minS);
