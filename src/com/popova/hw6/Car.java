f9d8d0c8dd256e163119db0b0f6001f47b47796b
Kate popova <katenysh98@gmail.com>
2021-11-11 21:53:35 +0200

    corrected hw5

---
+++ src/com/popova/hw6/Car.java
@@ -0,0 +1,83 @@
+package com.popova.hw6;
+
+public class Car {
+
+    private Manufacturer manufacturer;
+    private Engine engine;
+    private String colour;
+    private int petrol;
+
+    public Car(Manufacturer manufacturer, Engine engine, String colour) {
+        this.manufacturer = manufacturer;
+        this.engine = engine;
+        this.colour = colour;
+        this.petrol = 100;
+    }
+
+    public Manufacturer getManufacturer() {
+
+        return manufacturer;
+    }
+
+    public void setManyfacturer(Manufacturer manufacturer) {
+
+        this.manufacturer = manufacturer;
+    }
+
+    public Engine getEngine() {
+
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+
+        this.engine = engine;
+    }
+
+    public String getColour() {
+
+        return colour;
+    }
+
+    public void setColour(String colour) {
+
+        this.colour = colour;
+    }
+
+    public int getPetrol() {
+
+        return petrol;
+    }
+
+    public void setPetrol(String petrol) {
+
+        petrol = petrol;
+    }
+
+    public void startEngine(String name) {
+
+        if (isEnoughPetrolLevel()) {
+            System.out.println(name + "запустил двигатель");
+        } else {
+            System.out.println(name + "  НЕ запустил двигатель");
+        }
+    }
+
+    public boolean isEnoughPetrolLevel() {
+        return this.petrol > 50;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "manufacturer=" + manufacturer +
+                ", engine=" + engine +
+                ", colour='" + colour + '\'' +
+                ", petrol=" + petrol +
+                '}';
+    }
+}
+
+
+
+
---
+++ src/com/popova/hw6/Garage.java
@@ -0,0 +1,22 @@
+package com.popova.hw6;
+
+public class Garage {
+
+    public static void checkCar(Car car) {
+
+        if (car.isEnoughPetrolLevel() && car.getEngine().getPower() > 200) {
+            System.out.println("Проверка пройдена.");
+        } else if (car.isEnoughPetrolLevel() != car.getEngine().getPower() > 200) {
+            System.out.println("Не хватает мощности двигателя.");
+        } else if (car.getEngine().getPower() > 200 != car.isEnoughPetrolLevel()) {
+            System.out.println("Не хватает бензина.");
+        } else {
+            System.out.println("Не хватает бензина и мощности двигателя.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Garage{}";
+    }
+}
