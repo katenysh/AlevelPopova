91ff0dc9aaca9d9dc388fe80dbe09eb179db288b
Kate Popova <93034028+katenysh@users.noreply.github.com>
2021-11-17 14:40:38 +0200

    complete module 1

--- src/com/popova/module1/task2/Lock.java
+++ src/com/popova/module1/task2/Lock.java
@@ -5,7 +5,7 @@ public class Lock {
     private int[] array;
 
     public Lock(int[] array) {
-        this.array = array.clone();
+        this.array = array == null ? null : array.clone();
     }
 
     public int[] getArray() {
